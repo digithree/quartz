ea66d959ac9e58833cc1be143163f65cb3a3e420
Simon Kenny <skenwrites@gmail.com>
2025-06-07 15:32:21 +0100

    Remove all breadcrumbs

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,12 +16,7 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.DesktopOnly(
-      Component.ConditionalRender({
-        component: Component.Breadcrumbs(),
-        condition: (page) => page.fileData.slug !== "index",
-      }),
-    ),
+    //Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -50,7 +45,10 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+    //Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta()],
   left: [
     Component.DesktopOnly(Component.PageTitle()),
     Component.MobileOnly(Component.Spacer()),
